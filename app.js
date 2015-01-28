67c045ceb653673d48f7300c40c2122df82206d4
Alice Kindheart <moonstonecowgirl@gmail.com>
2015-01-28 18:08:03 -0500

    Made it to "Live Test". So far, so good!

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 var express = require( 'express' );
+var morgan = require('morgan');
 var app = express();
-var morgan = require( 'morgan');
+var swig = require( 'swig');
+
+var routes = require('./routes/');
+app.use('/', routes);
 
 app.use(morgan('dev'));
 
-// :method :url :status :response-time ms - :res[content-length]
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 
-app.get('/', function (req, res) {
-  res.send('Welcome to the edited page!')
-})
+app.use(express.static(__dirname + '/public'));
 
-app.get('/tweets', function (req, res) {
-  res.send('Page does not exist.')
-})
+//turns off caching
+swig.setDefaults({ cache: false });
 
 var server = app.listen(3000, function () {
 
@@ -22,3 +25,34 @@ var server = app.listen(3000, function () {
   console.log('server listening', host, port)
 
 })
+
+var router = express.Router();
+// could use one line instead: var router = require('express').Router();
+var tweetBank = require('./tweetBank');
+
+router.get('/', function (req, res) {
+  var tweets = tweetBank.list();
+  res.render( 'index', { title: 'Twitter.js', tweets: tweets } );
+});
+
+module.exports = router;
+
+
+// app.get('/', function (req, res) {
+//   var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+// res.render( 'index', {title: 'Hall of Fame', people: people} );
+
+// })
+
+// app.get('/news', function (req, res) {
+//    res.send('This is the news!')
+// })
+
+// app.get('/tweets/5', function (req, res) {
+//    res.send('This is the tweet!')
+// })
+
+
+//this is a comment I added
+
+//:method :url :status :response-time ms - :res[content-length]
